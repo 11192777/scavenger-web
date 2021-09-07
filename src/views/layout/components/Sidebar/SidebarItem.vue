83f35f8c2bfadeab9e39b5811919c93a3a3a235b
QingyuMeng <57242255+11192777@users.noreply.github.com>
2021-09-07 16:36:52 +0800

    fix:禁用菜单权限

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,39 +1,30 @@
 <template>
-  <div v-if="hasShowMainMenu(item)" class="menu-wrapper">
+  <div class="menu-wrapper">
     <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
-      <app-link :to="resolvePath(onlyOneChild.path)">
+      <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item v-if="onlyOneChild.meta" :icon="onlyOneChild.meta.icon||item.meta.icon" :title="onlyOneChild.meta.title" />
+          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />
         </el-menu-item>
       </app-link>
     </template>
 
-    <el-submenu v-else :index="resolvePath(item.path)">
+    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta.icon" :title="item.meta.title" />
+        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
       </template>
-
-      <template v-for="child in item.children" v-if="hasShowSubMenu(child)">
-        <sidebar-item
-          v-if="child.children&&child.children.length>0"
-          :is-nest="true"
-          :item="child"
-          :key="child.path"
-          :base-path="resolvePath(child.path)"
-          class="nest-menu" />
-        <app-link v-else :to="resolvePath(child.path)" :key="child.name">
-          <el-menu-item :index="resolvePath(child.path)">
-            <item v-if="child.meta" :icon="child.meta.icon" :title="child.meta.title" />
-          </el-menu-item>
-        </app-link>
-      </template>
+      <sidebar-item
+        v-for="child in item.children"
+        :key="child.path"
+        :is-nest="true"
+        :item="child"
+        :base-path="resolvePath(child.path)"
+        class="nest-menu"
+      />
     </el-submenu>
-
   </div>
 </template>
 
 <script>
-import SecurityApi from '@/api/security'
 import path from 'path'
 import { isExternal } from '@/utils'
 import Item from './Item'
@@ -43,7 +34,6 @@ export default {
   name: 'SidebarItem',
   components: { Item, AppLink },
   props: {
-    // route object
     item: {
       type: Object,
       required: true
@@ -58,51 +48,33 @@ export default {
     }
   },
   data() {
-    return {
-      onlyOneChild: null,
-      hideMenuList: null,
-      secondHideMenuList: null
-    }
+    this.onlyOneChild = null
+    return {}
   },
   created() {
     this.getSecurity()
   },
   methods: {
-    hasShowMainMenu(item) {
-      if((item.hidden && item.children) || this.hideMenuList.includes(item.name)) {
-        return false;
-      }
-      return true;
-    },
-    hasOneShowingChild(children, parent) {
+    hasOneShowingChild(children = [], parent) {
       const showingChildren = children.filter(item => {
         if (item.hidden) {
           return false
         } else {
-          // Temp set(will be used if only has one showing child)
           this.onlyOneChild = item
           return true
         }
       })
 
-      // When there is only one child router, the child router is displayed by default
       if (showingChildren.length === 1) {
         return true
       }
 
-      // Show parent if there are no child router to display
       if (showingChildren.length === 0) {
         this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
         return true
       }
 
       return false
-    },
-    hasShowSubMenu(item) {
-      if(item.hidden || this.secondHideMenuList.includes(item.name)) {
-        return false;
-      }
-      return true;
     },
     resolvePath(routePath) {
       if (this.isExternalLink(routePath)) {
@@ -112,12 +84,6 @@ export default {
     },
     isExternalLink(routePath) {
       return isExternal(routePath)
-    },
-    getSecurity() {
-      SecurityApi.getSecurity().then( res => {
-        this.hideMenuList = res.data.hideMenuList
-        this.secondHideMenuList = res.data.secondHideMenuList;
-      })
     }
   }
 }
